735b6bd86a9caeccf5e70c319d1cd78b2628455e
moses-MacBook <moseschanx@gmail.com>
2022-05-14 20:45:56 +0800

    added challenge info

--- main.c
+++ main.c
@@ -9,3 +9,95 @@
 #include <assert.h>
 #include <ctype.h>
 
+// Section 14 Challenge #1
+/*
+
+    Write a program to print the value of the following 
+    predefined symbolic constant 
+    __LINE__
+    __FILE__
+    __DATE__
+    __TIME__
+    __STDC__
+
+#2
+    Write a program that defines a macro that accepts two parameters 
+    and return the sum of the given numbers
+    #define MACRO_NAME(params) MACRO_BODY
+    Your program should have the user enter the two numbers
+#3
+    Create SQUARE and CUBE macro
+    Your program should have the user enter any number
+    Your program should then dispaly the square and cube of the number
+    as output by invoking the above macros
+
+    Example output:
+    Enter any number to find square and cube : 10
+    SQUARE(10) =  100
+    CUBE(10) = 1000
+
+#4
+    Write a C program to check whether a character is uppercase or lowercase
+    using macros
+
+    You should create two macros that accept a single argument (character)
+    IS_UPPER
+    IS_LOWER
+    both of the macros should return a boolean
+        true(1) or false(0) based on whether they are upper or lower case
+
+    Your macro will need to include a conditional and can use logical operators
+
+    Your program should have the user enter any character (getchar())
+
+    Your program should then display whether the character is upper or lower
+    case as ouput by invoking the above macros
+
+    Example output:
+    Enter any character : C
+    'C' is uppercase
+
+    Enter nay character :8
+    Entered character is not in the alphabet
+
+    ## additional enhancements
+    Feel free to add the below macros to perform additional checks on
+    characters
+
+    logic to check alphabtic digits alplhanumeric , vowels, consonants,
+    special characters , white space etc.
+        some of the above macros can use the other macros
+            an IS_ALPHANUMERIC macro could use an IS_ALPHABET and IS_DIGIT
+            marcros
+
+*/
+
+//int sec14_cha1to4()
+int main()
+{
+    //Challenge #1
+
+    //Challenge #1
+
+    //Challenge #2
+
+    //Challenge #2
+    
+    //Challenge #3
+
+    //Challenge #3
+
+    //Challenge #4
+
+    //Challenge #4
+    
+
+    return 0;
+
+}
+
+
+
+
+
+    
