9e44e054f2d47c1cd1749dde6df55fdd0678a6ec
moses-MacBook <moseschanx@gmail.com>
2022-05-14 11:40:32 +0800

    commit

--- exmaples.c
+++ exmaples.c
@@ -1,3 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <complex.h>
+#include <math.h>
+#include <stdbool.h>
+#include <setjmp.h>
+#include <assert.h>
+#include <ctype.h>
+
 
 void sizeCheck(){
 
