2c26b23db7df338aa9ac035ffb68c9770759d753
moses-MacBook <moseschanx@gmail.com>
2022-05-16 18:49:51 +0800

    xxx

--- main.c
+++ main.c
@@ -9,162 +9,118 @@
 #include <assert.h>
 #include <ctype.h>
 
-    #define CONCAT(a,b) a##b
-    #define WARNING(...) fprintf(stderr,__VA_ARGS__)
-    #define PUTS(param) (puts(#param))
-    #define MAKE_FUNCTION(NAME) void print_##NAME(char* str){\
-                                puts("In fucntion : print_"#NAME );\
-                                printf("Your parameter is : %s ",str);\
-                                }
-    #define MAKE_FUNCTION_DECL(NAME) void print_##NAME()
-    #define MAKE_FUNCTION_CALL(NAME,STR) print_##NAME(STR)
 
-// Section 14 Challenge #1
+// Section 15 Challenge #1 to 4
 /*
+    #1 jjjj
+        Manually compile and run your code
+            Compile many times with multiple options
+                try changing the object name  :
+                add debugging information :
+                add warnings : 
+                display warning as errors :
+    #2
+        Fix the bugs in provided code (udemy )
+            using fprintf(stderr,...) , or macro instead 
+            set debug level
 
-    Write a program to print the value of the following 
-    predefined symbolic constant 
-    __LINE__
-    __FILE__
-    __DATE__
-    __TIME__
-    __STDC__
+            using conditional compilation ( -D DEBUG)
+    #3
+        compile with -g
+        use debugger to figure out the problems in provided code
 
-#2
-    Write a program that defines a macro that accepts two parameters 
-    and return the sum of the given numbers
-    #define MACRO_NAME(params) MACRO_BODY
-    Your program should have the user enter the two numbers
-#3
-    Create SQUARE and CUBE macro
-    Your program should have the user enter any number
-    Your program should then dispaly the square and cube of the number
-    as output by invoking the above macros
+    #4
+        debugging with core dumps 
+            1 . set core dump size limit : ulimit -c ulimited
+        debugging with core dump files in gdb ( run backtrace to analy the core file)
 
-    Example output:
-    Enter any number to find square and cube : 10
-    SQUARE(10) =  100
-    CUBE(10) = 1000
+        
+*/
 
-#4
-    Write a C program to check whether a character is uppercase or lowercase
-    using macros
+int sum(int x, int y, int z) {
+  char c = 2;
+  int *a = NULL;
 
-    You should create two macros that accept a single argument (character)
-    IS_UPPER
-    IS_LOWER
-    both of the macros should return a boolean
-        true(1) or false(0) based on whether they are upper or lower case
+  fprintf(stderr, "x=%d\n", x);
+  fprintf(stderr, "y=%d\n", y);
+  fprintf(stderr, "z=%d\n", z);
+  fprintf(stderr, "a=%ld\n", (long)a);
+  
+  *a = 5;
 
-    Your macro will need to include a conditional and can use logical operators
+  fprintf(stderr, "*a=%d\n", *a);
 
-    Your program should have the user enter any character (getchar())
+  return (c + x + y + z + *a) / 3;
+}
 
-    Your program should then display whether the character is upper or lower
-    case as ouput by invoking the above macros
+//int sec15_cha1()
+int main(int argc, char *argv[]) {
+  int i, j, k;
+  int result;
 
-    Example output:
-    Enter any character : C
-    'C' is uppercase
+  fprintf(stderr, "Number of parameters = %d\n", argc);
 
-    Enter nay character :8
-    Entered character is not in the alphabet
+  if (argc == 1) {
+     printf("Please specify three numbers as parameters.\n");
+     exit(1);
+  }
 
-    ## additional enhancements
-    Feel free to add the below macros to perform additional checks on
-    characters
+  i = atoi(argv[1]);
 
-    logic to check alphabtic digits alplhanumeric , vowels, consonants,
-    special characters , white space etc.
-        some of the above macros can use the other macros
-            an IS_ALPHANUMERIC macro could use an IS_ALPHABET and IS_DIGIT
-            marcros
+  fprintf(stderr, "i=%d\n", i);
+
+  j = atoi(argv[2]);
+
+  fprintf(stderr, "j=%d\n", j);
+
+  k = atoi(argv[3]);
+
+  fprintf(stderr, "k=%d\n", k);
+
+  result = sum(i,j,12) + sum(j,k,19) + sum(i,k,13);
+
+  printf("%d\n", result);
+
+  return 0;
+}
+
+
+/* main without debuggin 
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int sum(int x, int y, int z) {
+  char c = 2;
+  int *a;
+
+  *a = 5;
+
+  return (c + x + y + z + *a) / 3;
+}
+
+int main(int argc, char *argv[]) {
+  int i, j, k;
+  int result;
+
+  if (argc == 1) {
+     printf("Please specify three numbers as parameters.\n");
+     exit(1);
+  }
+
+  i = atoi(argv[1]);
+  j = atoi(argv[2]);
+  k = atoi(argv[3]);
+
+  result = sum(i,j,12) + sum(j,k,19) + sum(i,k,13);
+
+  printf("%d\n", result);
+
+  return 0;
+}
+x
+465554654455        
+
 
 */
-MAKE_FUNCTION_DECL(MOSESCHAN);
-//int sec14_cha1to4()
-int main()
-{
-    /*Challenge #1
-    printf("__LINE__ is : %d\n",__LINE__);
-    printf("__FILE__ is : %s\n",__FILE__);
-    printf("__DATE__ is : %s\n",__DATE__); 
-    printf("__TIME__ is : %s\n",__TIME__);
-    printf("__STDC__ is : %d\n",__STDC__);
-    */
-
-    /*Challenge #2
-#define SUM_OF_TWO(param1,param2) ((param1)+(param2))
-
-    	int num1,num2;
-	printf("Please input two numer to get sum (by using macro) :");
-	scanf("%d%d",&num1,&num2);
-
-	printf("Your sum is : %d",SUM_OF_TWO(num1,num2));
-
-    */
-    
-    /*Challenge #3
-#define CUBE(param) ((param)*(param)*(param))
-#define SQUARE(param) ((param)*(param))
-   	int num3; 
-	printf("Enter any number to find square and cube :");
-	scanf("%d",&num3);
-
-	printf("Square of %d is : %d \n",num3,SQUARE(num3));
-	printf("Cube of %d is : %d \n",num3,CUBE(num3));
-
-
-    */
-
-
-    //Challenge #4
-#define IS_UPPER(param) (((param)<=90 && (param)>=65) ? 1 : 0)
-#define IS_LOWER(param) (((param)<=122 && (param)>=97) ? 1 : 0)
-#define CHECK(param) ()
-
-        MAKE_FUNCTION_CALL(MOSESCHAN,"DAMN SHIT");
-        
-
-    //
-    
-
-    return 0;
-
-}
-
-
-MAKE_FUNCTION(MOSESCHAN)
-
-
-#define COMMAND(NAME) {#NAME,NAME##_command}
-
-
-struct command{
-    char* name;
-    void (*function)(void);
-}
-
-struct command commands[]={
-    {"quit",quit_command},
-    {"exit",exit_command},
-    {"draw",draw_command}
-    COMMAND(help)
-    
-};
-
-
-#define DeclarSort(prefix , type) \
-static int _DeclareSort_ ## prefix ## _Compare(const void *a, const void *b){\
-    const type *aa; const type *bb; \
-    aa = a; bb = b; \
-    if(aa<bb) return -1;\
-        else if(bb<aa) return 1;\
-        else return 0;\
-}\
-void prefix##_sort(type *a , int n){\
-qsort(a,sizeof(type),n,_DeclareSort_## prefix ##_Compare);\
-}
-
-
-DeclarSort(int , int)
--- udemy_challenges.c
+++ udemy_challenges.c
@@ -739,3 +739,139 @@ int sec11_cha2() // problem at string reversal , maybe paltform issue?
     printf("Reversed string of %s is : %s \n",str,rev);
     return 0;
 }
+
+// Section 14 Challenge #1
+/*
+
+    Write a program to print the value of the following 
+    predefined symbolic constant 
+    __LINE__
+    __FILE__
+    __DATE__
+    __TIME__
+    __STDC__
+
+#2
+    Write a program that defines a macro that accepts two parameters 
+    and return the sum of the given numbers
+    #define MACRO_NAME(params) MACRO_BODY
+    Your program should have the user enter the two numbers
+#3
+    Create SQUARE and CUBE macro
+    Your program should have the user enter any number
+    Your program should then dispaly the square and cube of the number
+    as output by invoking the above macros
+
+    Example output:
+    Enter any number to find square and cube : 10
+    SQUARE(10) =  100
+    CUBE(10) = 1000
+
+#4
+    Write a C program to check whether a character is uppercase or lowercase
+    using macros
+
+    You should create two macros that accept a single argument (character)
+    IS_UPPER
+    IS_LOWER
+    both of the macros should return a boolean
+        true(1) or false(0) based on whether they are upper or lower case
+
+    Your macro will need to include a conditional and can use logical operators
+
+    Your program should have the user enter any character (getchar())
+
+    Your program should then display whether the character is upper or lower
+    case as ouput by invoking the above macros
+
+    Example output:
+    y C
+    'C' is uppercase
+
+    Enter nay character :8
+    Entered character is not in the alphabet
+
+    ## additional enhancements
+    Feel free to add the below macros to perform additional checks on
+    characters
+
+    logic to check alphabtic digits alplhanumeric , vowels, consonants,
+    special characters , white space etc.
+        some of the above macros can use the other macros
+            an IS_ALPHANUMERIC macro could use an IS_ALPHABET and IS_DIGIT
+            marcros
+
+*/
+int sec14_cha1to4()
+//int main()
+{
+    //Challenge #1
+    printf("__LINE__ is : %d\n",__LINE__);
+    printf("__FILE__ is : %s\n",__FILE__);
+    printf("__DATE__ is : %s\n",__DATE__); 
+    printf("__TIME__ is : %s\n",__TIME__);
+    printf("__STDC__ is : %d\n",__STDC__);
+    //
+
+    //Challenge #2
+#define SUM_OF_TWO(param1,param2) ((param1)+(param2))
+
+    	int num1,num2;
+	printf("Please input two numer to get sum (by using macro) :");
+	scanf("%d%d",&num1,&num2);
+
+	printf("Your sum is : %d",SUM_OF_TWO(num1,num2));
+
+    //
+    
+    //Challenge #3
+#define CUBE(param) ((param)*(param)*(param))
+#define SQUARE(param) ((param)*(param))
+   	int num3; 
+	printf("Enter any number to find square and cube :");
+	scanf("%d",&num3);
+
+	printf("Square of %d is : %d \n",num3,SQUARE(num3));
+	printf("Cube of %d is : %d \n",num3,CUBE(num3));
+
+
+    //
+
+
+    //Challenge #4
+#define IS_UPPER(param) (((param)<=90 && (param)>=65) ? 1 : 0)
+#define IS_LOWER(param) (((param)<=122 && (param)>=97) ? 1 : 0)
+#define IS_ALPHANUMERIC(param) (((param)<=57 && (param)>=48) ? 1 : 0)
+#define IS_CONTROL(param) (((param)<=31 && (param)>=0) ? 1 : 0)
+#define IS_SPACE(param) (((param == 32) ? 1 : 0))
+#define IS_NEWLINE(param) (((param == '\n') ? 1 : 0))
+
+
+#define CHECK(param) \
+if(IS_UPPER(param))\
+printf("%c is uppercase :",param);\
+else if(IS_LOWER(param))\
+printf("%c is lowercase :",param);\
+else if(IS_ALPHANUMERIC(param))\
+printf("%c is alphanumeric :",param);\
+else if(IS_CONTROL(param))\
+printf("%c is control :",param);\
+else if(IS_SPACE(param))\
+printf("%c is space :",param);\
+else if(IS_NEWLINE(param))\
+printf("%c is new line:",param);\
+else \
+printf("%c is not in the ASCII table :",param);\
+
+    char c;
+    printf("Enter nay character :");
+    scanf("%c",&c);
+    CHECK(c);
+
+    //
+    
+    return 0;
+
+}
+
+
