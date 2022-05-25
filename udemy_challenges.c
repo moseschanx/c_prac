8628d7297a26386ae7802d2e8783ed7fab07713a
debian-workstation <moseschanx@gmail.com>
2022-05-25 22:00:40 +0800

    added challengesz

--- udemy_challenges.c
+++ udemy_challenges.c
@@ -6,6 +6,7 @@
 #include <complex.h>
 #include <stdbool.h>
 #include <setjmp.h>
+#include <dlfcn.h>
 
 // Challenge #1 ( using variable length arrays )
 
@@ -1265,6 +1266,63 @@ void allocateMemory(void **ptr)
 
 }
 
+//StringFunctions.c
+
+#include <stdbool.h>
+#include <stdlib.h>
+#include "string_functions.h"
+
+int number_of_characters_in_string(char* str , char srchCha)
+{
+    char c ;
+    unsigned cnt = 0;
+    while((c = *str++) != '\0')
+        if(c == srchCha) ++cnt;
+    
+    return cnt;
+}
+
+
+bool remove_non_alpha_characters(char* src)
+{
+
+    char c;
+    while((c = *src++)!='\0')
+    {
+        if(!(c <= 'z' && c >= 'a' ||c <= 'Z' && c >= 'A'))
+            (*(src-1)) = ' ';
+
+    }
+    return 0;
+    
+}
+
+int length_of_string(char* src)
+{
+    unsigned len=0;
+    while(*src++ != '\0')
+        ++len;
+
+    return len;
+}
+
+bool str_cpy(char* src, char* dest)
+{
+    while(*src != '\0')
+        *dest++ = *src++;
+}
+
+bool sub_str(char* src, int i_start, int n, char* dest)
+{
+
+    for(;i_start!=0;--i_start,++src);
+    for(;n != 0 && *src != '\0';--n, *dest++ = *src++);
+    *dest = '\0';
+
+    return 0;
+
+}
+
 
 #include "string_functions.h"
 
@@ -1538,6 +1596,7 @@ int main()
   }
 
 
+  dlclose(handle); 
 
   return 0;
 
